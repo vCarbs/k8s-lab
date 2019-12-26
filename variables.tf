260527e3f71612d06b0f1fd7f5dcae07bae137e8
Nick Carbone <nick@vcarbs.com>
2019-12-26 13:42:27 -0800

    test branch

--- variables.tf
+++ variables.tf
@@ -1,7 +1,4 @@
-variable "config_context_cluster" {
+variable "kubernetes_cluster_name" {
   description = "This is the name of the kubernetes cluster you created. Use the 'kubectl cluster view' command to get this if you are unsure."
-}
-
-variable "metalip" {
-  description = "IP address block for Metal LB" 
+  default = "ntnxk8s"
 }
